8018389b5fc34c5c21e87e57736b8d2361f5ad26
rahat83384-ai <rahat83384@gmail.com>
2026-09-24 07:20:41 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'haker MD rahat,
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "haker MD rahat",
+  botOwner: 'haker MD rahat', // Your name
+  ownerNumber: '8801866583384', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.7",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://wa.me/8801341324640",
 };
 
 module.exports = settings;
